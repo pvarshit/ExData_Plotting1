613383b0bb022febb410b04ef6d7f48ac73d8c6b
pvarshit <147172818+pvarshit@users.noreply.github.com>
2025-06-23 17:13:48 +0530

    Update plot3.r

--- plot3.r
+++ plot3.r
@@ -1,15 +1,11 @@
-# Load required package
 library(sqldf)
 
-# Read only 1st and 2nd Feb 2007 data
 df <- read.csv.sql("household_power_consumption.txt",
                    sql = "SELECT * FROM file WHERE Date = '1/2/2007' OR Date = '2/2/2007'",
                    sep = ";", header = TRUE)
 
-# Combine Date and Time
 df$DateTime <- as.POSIXct(paste(df$Date, df$Time), format="%d/%m/%Y %H:%M:%S")
 
-# Plot sub-metering with weekday x-axis (using strptime)
 png("plot3.png", width=800, height=600)
 
 plot(df$DateTime, df$Sub_metering_1, type="l", col="black", 
@@ -17,7 +13,6 @@ plot(df$DateTime, df$Sub_metering_1, type="l", col="black",
 lines(df$DateTime, df$Sub_metering_2, col="red")
 lines(df$DateTime, df$Sub_metering_3, col="blue")
 
-# Use strptime for tick marks
 days <- strptime(c("2007-02-01", "2007-02-02", "2007-02-03"), format="%Y-%m-%d")
 axis(1, at = as.POSIXct(days), labels = format(days, "%a"))
 title(xlab = "Day")
